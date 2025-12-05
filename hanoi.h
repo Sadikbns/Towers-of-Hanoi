60e34a8a246aa543fb24a390420891700987db2f
Elseddik Bensadi <sadikbensadi7@gmail.com>
2025-12-05 14:43:52 +0100

    add hanoi.h file & delete main function from hanoi_iterative.c file

---
+++ hanoi.h
@@ -0,0 +1,7 @@
+#ifndef HANOI_H
+#define HANOI_H
+
+void hanoi_recursive(int n, char from, char to, char aux);
+void hanoi_iterative(int n, char from, char to, char aux);
+
+#endif
--- hanoi_iterative.c
+++ hanoi_iterative.c
@@ -37,37 +37,3 @@ void hanoi_iterative(int n, char start, char end, char aux) {
     
     free(stack);
 }
-
-// this will the same main function used to test recursive version of hanoi
-// (calculating time required to move 1 to n disks)
-int main() {
-    int n;
-    int i;
-    clock_t startTime;
-    clock_t endTime;
-    double seconds;
-
-    printf("Enter n: ");
-    scanf("%d", &n);
-
-    printf("\nNumber of disks | Execution time (seconds)\n");
-    printf("------------------------------------------\n");
-
-    for (i = 1; i <= n; i++) {
-
-        // on démarre la clock
-        startTime = clock();
-
-        hanoi_iterative(i, 'A', 'C', 'B');
-
-        // on stop la clock
-        endTime = clock();
-
-        // on calcule le temps total
-        seconds = (double)(endTime - startTime) / CLOCKS_PER_SEC;
-
-        printf("      %2d        |     %f\n", i, seconds);
-    }
-
-    return 0;
-}
